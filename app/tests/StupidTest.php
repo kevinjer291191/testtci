baf7fe439050c3dcc9a1b7b4fea4c1f997e3cd35
Kevin <Kevin.jeremy291191@gmail.com>
2016-04-08 18:15:55 +0700

    phpunit test

---
+++ app/tests/StupidTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace phpUnitTutorial\Test;
+
+class StupidTest extends \PHPUnit_Framework_TestCase
+{
+	public function testTrueIsTrue()
+	{
+	    $foo = true;
+	    $this->assertTrue($foo);
+	}
+}
+
+
+?>
