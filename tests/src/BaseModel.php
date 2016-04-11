86afdc0e40cb57ab9a3f8cfddccf9bb5871a096c
Kevin <Kevin.jeremy291191@gmail.com>
2016-04-11 20:04:46 +0700

    check

---
+++ tests/src/BaseModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HellobillTest\Src;
+
+abstract class BaseModel extends \PHPUnit_Framework_TestCase  
+{
+    public function functionok()
+    {
+        return "function ok";
+    }
+
+}
