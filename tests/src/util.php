8af77ce75def111459510600dc881dd3d21db691
Kevin <Kevin.jeremy291191@gmail.com>
2016-04-11 20:14:28 +0700

    asd

--- tests/src/util.php
+++ tests/src/util.php
@@ -1,6 +1,6 @@
 <?php
 
-//you can use this to generate array from json
+//you can use this to generate array 
 $string = file_get_contents("D:/xampp/htdocs/hellobill/fnb-services/tests/src/json-to-array.json");
 $array = json_decode($string, true);
 print_r($array);
